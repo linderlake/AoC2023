4d81ca21d65d23a83acfc79e3f1266dd78716ff3
Gustav Lindersjö <gustav.lindersjo@gmail.com>
2023-12-21 20:15:58 +0000

    day12 wip

---
+++ src/day12/solutions.cc
@@ -0,0 +1,16 @@
+#include "solutions.h"
+#include "placeholder.h"
+
+int PartOne(const std::vector<std::string>& input) {
+  int answer{};
+  for (const auto& row : input) {
+  }
+  return answer;
+}
+
+int PartTwo(const std::vector<std::string>& input) {
+  int answer{};
+  for (const auto& row : input) {
+  }
+  return answer;
+}
---
+++ src/day12/test/unit_tests.cc
@@ -0,0 +1,61 @@
+#include "AoCHelper.h"
+#include "solutions.h"
+#include "gtest/gtest.h"
+
+class Solutions : public ::testing::Test {
+protected:
+  std::vector<std::string> exampleInputPartOne{"#.#.### 1,1,3",
+                                               ".#...#....###. 1,1,3",
+                                               ".#.###.#.###### 1,3,1,6",
+                                               "####.#...#... 4,1,1",
+                                               "#....######..#####. 1,6,5",
+                                               ".###.##....# 3,2,1"};
+  std::vector<std::string> exampleInputPartTwo{};
+
+  std::function<std::vector<std::string>()> realInput = []() {
+    AoCHelper aocHelper{"2023", "12"};
+    return aocHelper.get_input();
+  };
+};
+
+TEST_F(Solutions, PartOneExampleData) {
+  auto answer{PartOne(exampleInputPartOne)};
+  EXPECT_EQ(answer, 142);
+}
+
+TEST_F(Solutions, PartOneExampleDataRowforRow) {
+  std::vector<int> answers{1, 4, 1, 1, 4, 10};
+  for (size_t i = 0; i < exampleInputPartOne.size(); i++) {
+    auto answer{PartOne({exampleInputPartOne[i]})};
+    EXPECT_EQ(answer, answers[i]);
+  }
+}
+/*
+TEST_F(Solutions, PartOneRealData) {
+  auto answer{PartOne(realInput())};
+  EXPECT_EQ(answer, 55447);
+}
+
+TEST_F(Solutions, PartTwoExampleData) {
+  auto answer{PartTwo(exampleInputPartTwo)};
+  EXPECT_EQ(answer, 281);
+}
+
+TEST_F(Solutions, PartTwoExampleDataRowforRow) {
+  std::vector<int> answers{29, 83, 13, 24, 42, 14, 76};
+  for (size_t i = 0; i < exampleInputPartTwo.size(); i++) {
+    auto answer{PartTwo({exampleInputPartTwo[i]})};
+    EXPECT_EQ(answer, answers[i]);
+  }
+}
+
+TEST_F(Solutions, PartTwoRealData) {
+  auto answer{PartTwo(realInput())};
+  EXPECT_EQ(answer, 72706);
+}
+*/
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+"
