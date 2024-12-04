07f1ced63926eb1aa74941c916f0ed377384c587
linderlake <gustav.lindersjo@gmail.com>
2024-12-04 12:02:41 +0000

    minor fixes in prep for aoc2024

--- tools/update_aochelper.sh
+++ tools/update_aochelper.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
-if [ ! -d "/home/vscode/AoCHelper" ]; then
-    echo "AoCHelper not found, cloning..."
-    git clone git@github.com:linderlake/AoCHelper.git /home/vscode/AoCHelper > /dev/null 2>&1 || echo "Failed to clone AoCHelper"
+
+REPO_URL="git@github.com:linderlake/AoCHelper.git"
+CLONE_DIR="/home/vscode/AoCHelper"
+
+if ! git ls-remote "$REPO_URL" &>/dev/null; then
+    echo "-> Failed to access the repository. Check your SSH keys for repo $REPO_URL"
+    exit 1
 fi
+
+if [ ! -d "$CLONE_DIR" ]; then
+    echo "AoCHelper not found, cloning..."
+    git clone "$REPO_URL" "$CLONE_DIR" || echo "-> Failed to clone AoCHelper"
+fi
+
 echo "Updating AoCHelper..."
-git -C /home/vscode/AoCHelper pull > /dev/null 2>&1 || echo "Failed to update AoCHelper"
+git -C "$CLONE_DIR" pull || echo "-> Failed to update AoCHelper"
 
 echo "Verifying symlinks..."
 mkdir -p ../lib/AocHelper
 if [ ! -L "../lib/AocHelper/libAoCHelper.a" ]; then
     echo "Creating symlink to libAoCHelper.a"
-    ln -s /home/vscode/AoCHelper/build/libAoCHelper.a ../lib/AocHelper/libAoCHelper.a 
+    ln -s "$CLONE_DIR/build/libAoCHelper.a" ../lib/AocHelper/libAoCHelper.a
 fi
 
 mkdir -p ../lib/AocHelper/include
 
 if [ ! -L "../lib/AocHelper/include/AoCHelper.h" ]; then
     echo "Creating symlink to AoCHelper.h"
-    ln -s /home/vscode/AoCHelper/src/AoCHelper.h ../lib/AocHelper/include/AoCHelper.h
+    ln -s "$CLONE_DIR/src/AoCHelper.h" ../lib/AocHelper/include/AoCHelper.h
 fi
 
 if [ ! -L "../lib/AocHelper/include/helper_functions.h" ]; then
     echo "Creating symlink to helper_functions.h"
-    ln -s /home/vscode/AoCHelper/src/helper_functions.h ../lib/AocHelper/include/helper_functions.h
+    ln -s "$CLONE_DIR/src/helper_functions.h" ../lib/AocHelper/include/helper_functions.h
 fi
